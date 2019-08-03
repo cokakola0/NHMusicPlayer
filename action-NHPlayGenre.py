491bfd59eebd8f795ddf680d8eac25ece1825fe0
cokakola0 <53462802+cokakola0@users.noreply.github.com>
2019-08-03 06:55:48 -0700

    Update action-NHPlayGenre.py

--- action-NHPlayGenre.py
+++ action-NHPlayGenre.py
@@ -1,1 +1,51 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
+import configparser
+from hermes_python.hermes import Hermes
+from hermes_python.ffi.utils import MqttOptions
+from hermes_python.ontology import *
+import io
+import vlc
+import time
+
+CONFIGURATION_ENCODING_FORMAT = "utf-8"
+CONFIG_INI = "config.ini"
+
+class SnipsConfigParser(configparser.SafeConfigParser):
+    def to_dict(self):
+        return {section : {option_name : option for option_name, option in self.items(section)} for section in self.sections()}
+
+
+def read_configuration_file(configuration_file):
+    try:
+        with io.open(configuration_file, encoding=CONFIGURATION_ENCODING_FORMAT) as f:
+            conf_parser = SnipsConfigParser()
+            conf_parser.readfp(f)
+            return conf_parser.to_dict()
+    except (IOError, configparser.Error) as e:
+        return dict()
+
+def subscribe_intent_callback(hermes, intentMessage):
+    conf = read_configuration_file(CONFIG_INI)
+    action_wrapper(hermes, intentMessage, conf)
+
+
+def NHPlayGenre(hermes, intentMessage, conf):
+    {{music_genre = intentMessage.slots.music_genre.first().value
+      playlist = os.listdir("/media/pi/Verbatim/music/genre/" + music_genre)
+      x = 0
+
+      while x < len(playlist):
+          song = str("/media/pi/Verbatim/music/genre/" + str(random.choice(playlist)) + ".mp3")
+          player = vlc.MediaPlayer(song)
+          player.play()
+          x += 1}}
+    {{/each}}
+
+
+if __name__ == "__main__":
+    mqtt_opts = MqttOptions()
+    with Hermes(mqtt_options=mqtt_opts) as h:
+        h.subscribe_intent("{{intent_id}}", subscribe_intent_callback) \
+         .start()
