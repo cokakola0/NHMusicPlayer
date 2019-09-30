2d4c0f7a6d9481a7545674cefff680d06f8a30f2
cokakola0 <53462802+cokakola0@users.noreply.github.com>
2019-09-30 14:41:50 -0700

    Update action-NHPlaySong.py

--- action-NHPlaySong.py
+++ action-NHPlaySong.py
@@ -6,26 +6,15 @@ import io
 import time
 import vlc
 
-def on_connect(client, userdata, flags, rc): 
-    print('Connected') 
-    mqtt.subscribe('hermes/intent/NHPlaySong')
-
 def on_message(client, userdata, msg):
-    # Parse the json response
-    intent_json = json.loads(msg.payload)
-    intentName = intent_json['intent']['intentName']
-    slots = intent_json['slots']
-    print('Intent {}'.format(intentName))
-    for slot in slots:
-        slot_name = slot['slotName']
-        raw_value = slot['rawValue']
-        value = slot['value']['value']
-        print('Slot {} -> \n\tRaw: {} \tValue: {}'.format(slot_name, raw_value, value))
-        
+    if msg.topic == 'hermes/intent/#':
+        payload = json.loads(msg.payload)
+        name = payload["intent"]["intentName"]
+        slots = payload["slots"]
+        artist_name = str(intentMessage.slots.artist_name.first().value)
+        song_name = str(intentMessage.slots.song_name.first().value)
+
 class PlaySong(song_name, artist_name):
-    song_name = intentMessage.slots.song_name.first().value
-    artist_name = intentMessage.slots.artist_name.first().value
-    
     sound = str("/media/pi/Verbatim1/music/artists/" + artist_name + "/all/" + song_name + ".mp3")
     
     player.play(sound)  
@@ -38,4 +27,3 @@ mqtt.on_connect = on_connect
 mqtt.on_message = on_message
 mqtt.connect('raspberrypi.local', 1883) 
 mqtt.loop_forever()
-
