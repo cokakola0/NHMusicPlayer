4741f6c28451ee788553da8600b7a26c9f6bda5e
cokakola0 <53462802+cokakola0@users.noreply.github.com>
2019-09-30 15:18:19 -0700

    Update action-NHPlaySong.py

--- action-NHPlaySong.py
+++ action-NHPlaySong.py
@@ -6,6 +6,7 @@ import io
 import time
 import vlc
 
+print("Test 1 passed")
 def on_message(client, userdata, msg):
     if msg.topic == 'hermes/intent/#':
         payload = json.loads(msg.payload)
@@ -14,16 +15,18 @@ def on_message(client, userdata, msg):
         artist_name = str(intentMessage.slots.artist_name.first().value)
         song_name = str(intentMessage.slots.song_name.first().value)
 
+print("Test 2 passed")
 class PlaySong(song_name, artist_name):
     sound = str("/media/pi/Verbatim1/music/artists/" + artist_name + "/all/" + song_name + ".mp3")
-    
+    print("Playing song...")
     player.play(sound)  
         
 if __name__ == "__main__":
     PlaySong()
         
-mqtt = mqtt.Client() 
-mqtt.on_connect = on_connect
-mqtt.on_message = on_message
-mqtt.connect('raspberrypi.local', 1883) 
-mqtt.loop_forever()
+print("Program is running correctly")
+#mqtt = mqtt.Client() 
+#mqtt.on_connect = on_connect
+#mqtt.on_message = on_message
+#mqtt.connect('raspberrypi.local', 1883) 
+#mqtt.loop_forever()
